869bec517fd76a27eb065978a3daa3b72a58e4bf
David <david.nunez@uptc.edu.co>
2022-05-16 16:18:28 -0500

    Version de prueba

---
+++ src/main/java/com/mycompany/abc_prototype/ABC_Prototype.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.abc_prototype;
+import com.mycompany.abc_prototype.forms.Formulario;
+/**
+ *
+ * @author David Felipe
+ */
+public class ABC_Prototype {
+
+    public static void main(String[] args) {
+        Formulario ventana = new Formulario();
+        ventana.show();
+        
+    }
+}
